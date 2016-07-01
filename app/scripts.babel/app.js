1dd61db1bd634f1edbf8d854af64655f427e79a3
Sammy Braun <sammy@brandwatch.com>
2016-07-01 11:12:46 +0200

    Added github injection to initiate on changes

--- app/scripts.babel/app.js
+++ app/scripts.babel/app.js
@@ -1,6 +1,13 @@
 import FileFilter from './file-filter';
+import injection from 'github-injection';
 
-if (window.location.pathname.match(/pull\/[0-9]+\/files$/)) {
-  const filter = new FileFilter();
-  filter.init();
-}
+injection(window, function(err) {
+  if (err) {
+    throw err;
+  }
+
+  if (window.location.pathname.match(/pull\/[0-9]+\/files$/) || window.location.pathname.match(/pull\/[0-9]+\/commits\/[0-9a-z]+$/)) {
+    const filter = new FileFilter();
+    filter.init();
+  }
+});
